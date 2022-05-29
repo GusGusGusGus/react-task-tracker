b4f5684463079343a461a42af68ad890ed009e11
QuickTriXXX <isthisreal@whatever.com>
2022-05-29 09:26:19 +0100

    created addTask and toggleReminder methods in parent, added AddTask form component, added double click to toggle reminder in each task component, added toggle funcionality to Add button on Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
-import React from "react";
+import { useState } from "react";
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from "react";
+import AddTask from './components/AddTask'
 
 
 
 const App = () => {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -29,18 +29,37 @@ const App = () => {
     },
 ])
 
+//add task
+const addTask = (task) => {
+  //id mockup
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask]) 
+}
+
 //delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 } 
 
+//toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+}
 
   return (
     <div className="container">
-      <Header />
+      <Header 
+        onAdd={ () => setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask} 
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {
         tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask}/> : 
+        <Tasks tasks={tasks} 
+          onDelete={deleteTask} 
+          onToggle={toggleReminder} /> : 
         'No tasks to show'
       }
       
